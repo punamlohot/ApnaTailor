56be1362b3f146c4e31d225853cdcd5b3f26abbe
punam lohot <punamlohot4@gmail.com>
2023-12-15 14:19:33 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import AllRoutes from "./routes/allRoutes";
+import "./assets/scss/Theme.scss";
+import "./constants/style.css";
+
+const App = () => {
+  return (
+    <>
+      <React.Fragment>
+        <AllRoutes />
+      </React.Fragment>
+    </>
+  );
+};
+
+export default App;
